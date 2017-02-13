6dacf4d1f0610d44da44f70b879d2084710989bc
Rui Cheng <rui.cheng@infotrack.com.au>
2017-02-13 18:59:52 +1100

    initial commit

---
+++ src/PreviewAspNetCoreWebAPI/LocalEntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace PreviewAspNetCoreWebAPI
+{
+    /// <summary>
+    /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+    /// </summary>
+    public class LocalEntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                // .UseIISIntegration()
+                .UseUrls("http://*:5000")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
